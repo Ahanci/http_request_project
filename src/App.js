051dbd90a4a7eeb3a4fcfe7bcc8bf8d6f1f11309
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-08-11 17:35:38 +0300

    firebase_used_for_adding_movie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import AddMovie from './components/AddMovie'; 
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -16,24 +17,25 @@ function App() {
     setError(null);
     
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-eda62-default-rtdb.firebaseio.com/movies.json');
       if(!response.ok){
         throw new Error('Something went wrong...')
       } // Bu eğer json() un altında olursa Unexpected token {'<', " <!DOCTYPE "... is not valid JSON} böyle bir hata verdi yani gelmeyen veriyi json a dönüştürmek istiyor.
       const data = await response.json();
+      const loadedMovies=[];
       
+      for(const key in data){
+        loadedMovies.push(
+          {
+            id:key,
+            title: data[key].title,
+            openingText:data[key].openingText,
+            releaseDate:data[key].releaseDate,
+          }
+        )
+      }
       
-      const transformedMovie = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-
-        };
-
-      });
-      setMovies(transformedMovie);
+      setMovies(loadedMovies);
       setIsLoading(false);
 
     }
@@ -46,6 +48,19 @@ function App() {
   useEffect( ()=>{
     fetchMoviesHandler();
   }, [fetchMoviesHandler] );
+
+  const addMovieHandler=  async (movie)=>{
+   const response= await fetch('https://react-http-eda62-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-Type':'application/json'
+      }
+      });
+    const data= await response.json();
+    console.log(data);
+  };
+
 
   let content= <p> Found No Movies</p>
   if(movies.length>0){
@@ -60,6 +75,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
