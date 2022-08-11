92289d7adb1dd51b0fd895097526f913b1c1288e
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-08-11 13:09:00 +0300

    loading_spinner_Added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const[isLoading, setIsLoading]=useState(false);
+
   const fetchMoviesHandler= async ()=>{
+    setIsLoading(true);
     const response =await fetch('https://swapi.dev/api/films/');
     const data =await response.json();
     const transformedMovie= data.results.map( (movieData)=>{
@@ -20,22 +23,7 @@ function App() {
       
     } );
     setMovies(transformedMovie); 
-    // fetch('https://swapi.dev/api/films/').then((response)=>{
-    //   return response.json();
-    // }).then( (data)=>{
-    //   const transformedMovie=data.results.map( (movieData)=>{
-    //     return{
-    //       id: movieData.episode_id,
-    //       title: movieData.title,
-    //       openingText: movieData.opening_crawl,
-    //       releaseDate: movieData.release_date,
-          
-    //     };
-        
-    //   }  );
-    //   setMovies(transformedMovie);
-    // } )
-    
+    setIsLoading(false);    
   }
 
   return (
@@ -44,7 +32,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}       
+        {isLoading && <p> Loading Your Films...</p>}
       </section>
     </React.Fragment>
   );
